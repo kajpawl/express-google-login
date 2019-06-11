09d85ba69670dc8c17595599715bb05ba236a020
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2019-06-11 20:15:20 +0200

    Create server with G+ login

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '351195826393-gv6ksql4qkouk8f912hipvnrt1divqub.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'GYOAOF5SBQb_osIRhmlzVaQd',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
